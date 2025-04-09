bb7092a65d698f547a08b33c176906c2599f81f5
mohamedHany2105 <mohamed2105hany@gmail.com>
2025-04-09 14:40:22 +0200

    sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-const signUp = async (req, res, next) => {
+import jwt from "jsonwebtoken";
+export const signUp = async (req, res, next) => {
     console.log(req.body)
     const { name, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10)
-    const newUser = new User({name,email, password: hashedPassword })
+    const newUser = new User({ name, email, password: hashedPassword })
     try {
 
         await newUser.save();
@@ -21,4 +22,27 @@ const signUp = async (req, res, next) => {
 
 }
 
-export default signUp
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        // console.log({email,password});
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "user not found"));
+        }
+            const validPassword = bcrypt.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler('401', "wrong credentials or password"))
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET,);
+
+        const {password:pass,...rest}=validUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true })
+        .status(200).
+        json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+const handleChange=()=>{
 
+}
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+sign in
+      </h1>
+
+      <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+
+        </button>
+        </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+     </div>
+
+
+    </div>
+    </div>
   )
 }
